aa156b84bbaa7d778b690c4118c37a43e8fee5e6
B Caissotti di Chiusano <b.caissottidichiusano-1@student.tudelft.nl>
2025-05-26 18:46:30 +0200

    refactoring and start of adding automatic login

--- talkbankScraper.py
+++ talkbankScraper.py
@@ -6,6 +6,43 @@ from bs4 import BeautifulSoup
 import re
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import secrets
+
+
+class SeleniumBrowser:
+    driver = None
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def open_url(self, url, cookie_value):
+        self.driver.get(url)
+        cookie = {'name': 'talkbank', 'value': cookie_value, 'domain': 'sla.talkbank.org'}
+        self.driver.add_cookie(cookie)
+
+    def close_url(self):
+        self.driver.close()
+
+    def add_input(self, by: By, value: str, text: str):
+        field = self.driver.find_element(by, value)
+        field.send_keys(text)
+        time.sleep(1)
+
+    def click_button(self, by: By, value: str):
+        button = self.driver.find_element(by, value)
+        button.click()
+        time.sleep(1)
+
+    def login_talkbank(self, email: str, password: str):
+        self.click_button(by=By.CLASS_NAME, value="_button_14b4t_1 _blackWhite_14b4t_46")
+        self.add_input(by=By.ID, value="authModals_userName", text=email)
+        self.add_input(by=By.ID, value="authModals_pswd", text= password)
+        self.click_button(by=By.ID, value="authModals_loginBtn")
+
+    def show_metadata(self):
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        print(soup.prettify())
 
 
 def downloader(cookie_value, url):
@@ -35,17 +72,9 @@ def downloader(cookie_value, url):
         'Cookie': f'talkbank={cookie_value}'
     }
 
-
-    driver = webdriver.Chrome()
-    driver.get(url=url)
-    cookie = {'name' : 'talkbank', 'value' : cookie_value, 'domain': 'sla.talkbank.org'}
-    driver.add_cookie(cookie)
-
     time.sleep(30)
 
-    #page = requests.get(url, headers=headers)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    print(soup.prettify())
+    # page = requests.get(url, headers=headers)
 
     # TODO:
     # Save audio file name
