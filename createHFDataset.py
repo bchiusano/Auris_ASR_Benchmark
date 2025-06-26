e0d253897bb30322088d80132c8da1152351e8f8
B Caissotti di Chiusano <b.caissottidichiusano-1@student.tudelft.nl>
2025-06-26 16:02:41 +0200

    dataset uploaded to hf with subsets

--- createHFDataset.py
+++ createHFDataset.py
@@ -8,12 +8,11 @@ import os
 import sys
 
 
-def upload(hf_audios, hf_titles, six_audio_utterances):
-
-    audio_dataset = Dataset.from_dict(
-        {"audio": hf_audios, "file_name": hf_titles, "transcript": six_audio_utterances}).cast_column("audio",
-                                                                                                      Audio())
-    audio_dataset.push_to_hub("bchiusano/WrongPatternsCHILDES")
+def upload(dataset_repo, subset_name, hf_audios, hf_titles, audio_utterances):
+    audio_dataset = Dataset.from_dict({"audio": hf_audios,
+                                       "file_name": hf_titles,
+                                       "transcript": audio_utterances}).cast_column("audio", Audio())
+    audio_dataset.push_to_hub(dataset_repo, subset_name)
 
 
 class CreateHFData:
@@ -31,8 +30,10 @@ class CreateHFData:
         all_audio_utterances = []
         six_audio_utterances = []
 
-        hf_audios = []
-        hf_titles = []
+        hf_audios_all = []
+        hf_titles_all = []
+        hf_audios_six = []
+        hf_titles_six = []
 
         filenames = self.data_csv['filename'].to_list()
 
@@ -68,30 +69,27 @@ class CreateHFData:
                 end = int(stamp[1])
                 audio_chunk = audio[start:end]
 
-                # new_row = pd.DataFrame(
-                #    {"file_name": ["audio_{name}_{s}_{e}.mp3".format(name=title_name, s=start, e=end)],
-                #     "transcript": [utterance]})
-
                 save_title = "audio_{name}_{s}_{e}.mp3".format(name=title_name, s=start, e=end)
 
                 if len(utterance.split()) > 5:
                     path_to_audio = (self.audio_output + save_title).format(six="Six")
-                    #audio_chunk.export((self.audio_output + save_title).format(six="Six"), format="mp3")
+                    # audio_chunk.export(path_to_audio, format="mp3")
 
                     six_audio_names.append(save_title)
-                    hf_audios.append(path_to_audio)
-                    hf_titles.append(cleaned_name)
                     six_audio_utterances.append(utterance)
-                    # six_metadata = pd.concat([six_metadata, new_row], ignore_index=True)
+                    hf_audios_six.append(path_to_audio)
+                    hf_titles_six.append(cleaned_name)
 
-                #audio_chunk.export((self.audio_output + save_title).format(six=""), format="mp3")
-                # all_metadata = pd.concat([all_metadata, new_row], ignore_index=True)
+                path_to_audio = (self.audio_output + save_title).format(six="")
+                # audio_chunk.export(path_to_audio, format="mp3")
                 all_audio_names.append(save_title)
                 all_audio_utterances.append(utterance)
+                hf_audios_all.append(path_to_audio)
+                hf_titles_all.append(cleaned_name)
 
-        #self.add_metadata(all_audio_names, all_audio_utterances, six_audio_names, six_audio_utterances)
+        # self.add_metadata(all_audio_names, all_audio_utterances, six_audio_names, six_audio_utterances)
 
-        return hf_audios, hf_titles, six_audio_utterances
+        return hf_audios_all, hf_titles_all, all_audio_utterances, hf_audios_six, hf_titles_six, six_audio_utterances
 
     def add_metadata(self, all_audio_names, all_audio_utterances, six_audio_names, six_audio_utterances):
 
@@ -109,17 +107,28 @@ class CreateHFData:
 
 
 path_to_audio_files = "CHILDES Downloads/DutchAfrikaans/Asymmetries/CK-TD/"
-path_to_audio_output = "datasets/wrongDataset{six}/Asymmetries/"
-path_to_metadata_output = "datasets/wrongDataset{six}/"
+paths_to_data = ["csvs/wrong_data.csv", "csvs/correct_data.csv"]
+paths_audio_output = ["datasets/wrongDataset{six}/Asymmetries/", "datasets/correctDataset{six}/Asymmetries/"]
+paths_metadata_output = ["datasets/wrongDataset{six}/", "datasets/correctDataset{six}/"]
+subsets = [["wrongPatterns", "wrongPatternsSix"], ["correctPatterns", "correctPatternsSix"]]
 
-data = pd.read_csv("csvs/wrong_data.csv")
+hf_dataset_repo = "bchiusano/AsymmetriesCHILDES"
 
-create_database = CreateHFData(path_audio=path_to_audio_output,
-                               data_csv=data,
-                               audio_output=path_to_audio_output,
-                               metadata_output=path_to_metadata_output,
-                               debug=True)
+for i in range(len(paths_to_data)):
+    data = pd.read_csv(paths_to_data[i])
+    path_to_audio_output = paths_audio_output[i]
+    path_to_metadata_output = paths_metadata_output[i]
+    subset = subsets[i]
 
-audios, names, transcripts = create_database.split_audio()
+    create_database = CreateHFData(path_audio=path_to_audio_output,
+                                   data_csv=data,
+                                   audio_output=path_to_audio_output,
+                                   metadata_output=path_to_metadata_output,
+                                   debug=True)
 
-upload(hf_audios=audios, hf_titles=names, six_audio_utterances=transcripts)
+    audios, names, transcripts, audios_six, names_six, transcripts_six = create_database.split_audio()
+
+    upload(dataset_repo=hf_dataset_repo, subset_name=subset[0], hf_audios=audios, hf_titles=names,
+           audio_utterances=transcripts)
+    upload(dataset_repo=hf_dataset_repo, subset_name=subset[1], hf_audios=audios_six, hf_titles=names_six,
+           audio_utterances=transcripts_six)
