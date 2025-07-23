601c1408348c4e84392a33eaf2d6281bd02b1611
B Caissotti di Chiusano <b.caissottidichiusano-1@student.tudelft.nl>
2025-07-23 17:26:06 +0200

    you can now filter through examples of the results for each example that was run

--- ui/app.py
+++ ui/app.py
@@ -1,15 +1,17 @@
 import gradio as gr
+import pandas as pd
+
 from normalizer.eval_utils import score_results
 from constants import TITLE, INTRODUCTION_TEXT, AURIS_ORIGINAL_DESCRIPTION, METRICS_TEXT, WER, RTFX, GITHUB_REPO
-
+import json
 
 _, _, all_df, composite_df = score_results("../results/")
-#_, _, all_df, composite_df = score_results("../results/small/")
-
-# TODO: User can see some examples of reference vs prediction
+# _, _, all_df, composite_df = score_results("../results/small/")
 
 numeric_wer = []
 numeric_rtfx = []
+
+unique_dataset = list(set(all_df['dataset']))
 
 for wer in all_df['WER']:
     numeric_wer.append(float(wer.strip().replace('%', '')))
@@ -21,9 +23,7 @@ with gr.Blocks() as demo:
     gr.Markdown(INTRODUCTION_TEXT)
 
     with gr.Tabs(elem_classes="tab-buttons") as tabs:
-
         with gr.TabItem("All Tests"):
-
             gr.Markdown("## Stats")
             with gr.Row():
                 gr.Label(f"{min(numeric_wer)}%", label="Lowest WER")
@@ -32,21 +32,40 @@ with gr.Blocks() as demo:
             gr.Markdown("## Benchmark")
             gr.DataFrame(all_df)
 
-            gr.Markdown("Filter Examples")
-            with gr.Row():
-                models = gr.Dropdown(composite_df['model'].tolist(), value="All", label="Models", allow_custom_value=True)
-
         with gr.TabItem("Composite Results"):
+            gr.Markdown("## Average per Model")
             gr.DataFrame(composite_df)
+
+        with gr.TabItem("Filter Examples"):
+            gr.Markdown("## Filter Examples")
+
+            with gr.Row():
+                all_models = composite_df['model'].tolist()
+                models = gr.Dropdown(all_models, value=all_models[0], label="Models", allow_custom_value=True,
+                                     interactive=True)
+                data = gr.Dropdown(unique_dataset, value=unique_dataset[0], label="Dataset", allow_custom_value=True,
+                                   interactive=True)
+
+            examples_df = gr.DataFrame(headers=['Original', 'Predicted'])
+
+
+            @gr.on(inputs=[models, data], outputs=examples_df)
+            def filtered_data(models, data):
+                j_file = f"../results/MODEL_{models.replace('/', '-')}_DATASET_{data}.jsonl"
+                try:
+                    j_df = pd.read_json(j_file, lines=True)
+                    return pd.DataFrame({"Original": j_df['text'], "Predicted": j_df['pred_text']})
+                except:
+                    return pd.DataFrame({"Original": ['work in progress'], "Predicted": ['work in progress']})
 
         with gr.TabItem("About the Project"):
             gr.Markdown(AURIS_ORIGINAL_DESCRIPTION)
             gr.Markdown(METRICS_TEXT)
-            #with gr.Row():
+            # with gr.Row():
             gr.Markdown(WER)
             gr.Markdown(RTFX)
             gr.Markdown(GITHUB_REPO)
 
-            gr.Markdown("## Notes and Suggestions")
+            gr.Markdown("## Datasets Used")
 
 demo.launch()
